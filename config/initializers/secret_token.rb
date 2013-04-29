2d5eee4109e79e7092342d3d1f0171c04b189c3c
Romina Vargas <rsocci@me.com>
2013-04-29 17:39:20 -0400

    Updated frontend for the new authentication system
    added simple_form
    updated several forms to bootstrap/simple_form
    added login/logout/register/profile links to our nav
    added a collection select to our status form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '3ac95443c3c2a83b472c3afff4768ef3ba00137d008005cc84f689ffa37ed505eee9cf5926e9699536527123a48d83f3c175aaf97354703d831e9f7733be6aa8'
